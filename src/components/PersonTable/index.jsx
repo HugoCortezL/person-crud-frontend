087ea8bdf791dbc528ef2b79507f5ce9bbb63f25
Hugo Cortez <hugo.cortez@t4i.intranet>
2022-04-18 20:52:49 -0300

    personTable dinamic data ready

--- src/components/PersonTable/index.jsx
+++ src/components/PersonTable/index.jsx
@@ -1,6 +1,21 @@
 import {Container, Badge} from './styles'
 import {BsPencilFill, BsFillTrashFill} from 'react-icons/bs'
+
+import {useState, useEffect} from 'react'
+
+import {getAllPersons} from '../../api/personAPI'
+
+
+
 export default function PersonTable() {
+    const [persons, setPersons] = useState([])
+
+    useEffect(() => {
+        getAllPersons().then((result) => {
+            setPersons(result)
+        })
+    }, [])
+
     return (
         <Container>
             <table>
@@ -16,56 +31,26 @@ export default function PersonTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Hugo Cortez</td>
-                        <td>hugocortezprog@gmail.com</td>
-                        <td>81991274586</td>
-                        <td>Masculino</td>
-                        <td>13/06/2000</td>
-                        <td>
-                            <Badge status={true}>
-                                True
-                            </Badge>
-                        </td>
-                        <td className="options">
-                            <BsPencilFill color="#F58100" size="20"/> 
-                            <BsFillTrashFill color="#FF1D23" size="20"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Hugo Cortez</td>
-                        <td>hugocortezprog@gmail.com</td>
-                        <td>81991274586</td>
-                        <td>Masculino</td>
-                        <td>13/06/2000</td>
-                        <td>
-                            <Badge status={false}>
-                                False
-                            </Badge>
-                        </td>
-                        <td className="options">
-                            <BsPencilFill color="#F58100" size="20"/> 
-                            <BsFillTrashFill color="#FF1D23" size="20"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Hugo Cortez</td>
-                        <td>hugocortezprog@gmail.com</td>
-                        <td>81991274586</td>
-                        <td>Masculino</td>
-                        <td>13/06/2000</td>
-                        <td>True</td>
-                        <td>Edit - Delete</td>
-                    </tr>
-                    <tr>
-                        <td>Hugo Cortez</td>
-                        <td>hugocortezprog@gmail.com</td>
-                        <td>81991274586</td>
-                        <td>Masculino</td>
-                        <td>13/06/2000</td>
-                        <td>True</td>
-                        <td>Edit - Delete</td>
-                    </tr>
+                    {
+                        persons.map(person => (
+                            <tr key={person.Id}>
+                                <td>{person.firstname} {person.lastname}</td>
+                                <td>{person.email}</td>
+                                <td>{person.phone}</td>
+                                <td>{person.gender}</td>
+                                <td>{person.birthdate}</td>
+                                <td>
+                                    <Badge status={person.active}>
+                                        {person.active}
+                                    </Badge>
+                                </td>
+                                <td className="options">
+                                    <BsPencilFill color="#F58100" size="20"/> 
+                                    <BsFillTrashFill color="#FF1D23" size="20"/>
+                                </td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </Container>
